190affac71914fcdb1d4495ea9d8f6174627ad8d
Victor Marin <victormarinviloria@gmail.com>
2022-07-02 08:36:22 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const rules = document.getElementById('rules');
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
+let score = 0;
+
+const brickRowCount = 9;
+const brickColumnCount = 5;
+
 // create ball props
 const ball = {
     x: canvas.width / 2,
@@ -14,17 +19,7 @@ const ball = {
     dy: -4
 }
 
-// draw ball on canvas
-function drawBall() {
-    ctx.beginPath();
-    ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI *2);
-    ctx.fillStyle = '#0095dd';
-    ctx.fill();
-    ctx.closePath();
-}
-
 // create paddle props
-
 const paddle = {
     x: canvas.width / 2 - 40,
     y: canvas.height -20,
@@ -32,7 +27,26 @@ const paddle = {
     h: 10,
     speed: 8,
     dx: 0
+};
+
+// create brick props 
+const brickInfo = {
+    w: 70,
+    h: 20,
+    padding: 10,
+    offsetX: 45,
+    offsetY: 60,
+    visible: true
 }
+
+// draw ball on canvas
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI *2);
+    ctx.fillStyle = '#0095dd';
+    ctx.fill();
+    ctx.closePath();
+};
 
 //draw paddle on canvas
 function drawPaddle() {
@@ -43,8 +57,129 @@ function drawPaddle() {
     ctx.closePath();
 }
 
-drawBall();
-drawPaddle();
+// create bricks
+const bricks = [];
+for (let i = 0; i < brickRowCount; i++) {
+    bricks[i] = [];
+    for(let j = 0; j < brickColumnCount; j++) {
+        const x = i * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX;
+        const y = j * (brickInfo.h + brickInfo.padding) + brickInfo.offsetY;
+        bricks[i][j] = { x, y, ...brickInfo }
+    }
+}
+
+// draw bricks on canvas
+function drawBricks() {
+    bricks.forEach(column => {
+        column.forEach(brick => {
+            ctx.beginPath();
+            ctx.rect(brick.x, brick.y, brick.w, brick.h);
+            ctx.fillStyle = brick.visible ? '#0095dd' : 'transparent';
+            ctx.fill();
+            ctx.closePath();
+        })
+    })
+}
+
+// move paddle on canvas
+function movePaddle() {
+    paddle.x += paddle.dx;
+
+    // wall detection
+    if(paddle.x + paddle.w > canvas.width) {
+        paddle.x = canvas.width - paddle.w;
+    }
+
+    if(paddle.x < 0) {
+        paddle.x = 0;
+    }
+}
+
+// move ball on canvas
+function moveBall() {
+    ball.x += ball.dx;
+    ball.y += ball.dy;
+
+    // wall collision (x)
+    if(ball.x + ball.size > canvas.width || ball.x - ball.size < 0) {
+        ball.dx *= -1;
+    }
+    // wall collision (top/bot)
+    if(ball.y + ball.size > canvas.height || ball.y - ball.size < 0) {
+        ball.dy *= -1;
+    }
+
+    // paddle collision
+    if(ball.x - ball.size > paddle.x && 
+        ball.x + ball.size < paddle.x + paddle.w && 
+        ball.y + ball.size > paddle.y) {
+        ball.dy = -ball.speed;
+    }
+
+    // brick collision
+    bricks.forEach(column => {
+        column.forEach(brick => {
+            if(brick.visible) {
+                
+            }
+        })
+    })
+}
+
+
+// draw everything
+function draw() {
+    // clear canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    drawBall();
+    drawPaddle();
+    drawScore();
+    drawBricks();
+}
+
+// draw score on canvas
+function drawScore() {
+    ctx.font = '20px Arial';
+    ctx.fillText(`Score: ${score}`, canvas.width - 100, 30);
+}
+
+//update canvas drawing and animation
+function update() {
+
+    movePaddle();
+    moveBall();
+
+    //draw everything
+    draw();
+
+    requestAnimationFrame(update);
+}
+
+update();
+
+// keydown event
+function keyDown(e) {
+    if(e.key === 'Right' || e.key === 'ArrowRight') {
+        paddle.dx = paddle.speed;
+    } else if(e.key === 'Left' || e.key === 'ArrowLeft') {
+        paddle.dx = -paddle.speed;
+    }
+}
+
+//keyup event
+function keyUp(e) {
+    if(e.key === 'Right' || e.key === 'ArrowRight' || e.key === 'Left'
+    || e.key === 'ArrowLeft') {
+        paddle.dx = 0;
+    }
+}
+
+// keyboard event handlers
+document.addEventListener('keydown', keyDown);
+document.addEventListe
+ner('keyup', keyUp);
+
 
 // rules and close event handlers
 rulesBtn.addEventListener('click', () =>
